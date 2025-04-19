7f6670a54b546c42c70348cebe5fd3a7df8353d1
leac1m <obemichael10@gmail.com>
2025-04-19 22:22:45 +0100

    Drawn a square in the canvas

--- src/core.js
+++ src/core.js
@@ -1,4 +1,7 @@
 "use strict";
+import * as vertexBuffer from './vertex_buffer.js'
+import * as simpleShader from './shader_support.js';
+
 let mGL = null;
 function getGL() { return mGL; }
 
@@ -12,13 +15,30 @@ function initWebGL(htmlCanvasID) {
         return;
     }
     mGL.clearColor(0.0, 0.8, 0.0, 1.0);
+
+    // initializing buffer wuth vertex positions for the unit square
+    vertexBuffer.init();
+
+    // load and compile the vertex and fragment shaders
+    simpleShader.init("VertexShader", "FragmentShader");
 }
 
 function clearCanvas() {
     mGL.clear(mGL.COLOR_BUFFER_BIT);
 }
 
+function drawSquare() {
+    // Activate the shader
+    simpleShader.activate();
+
+    // draw with the above settings
+    mGL.drawArrays(mGL.TRIANGLE_STRIP, 0, 4);
+}
+
 window.onload = function() {
     initWebGL("GLCanvas");
     clearCanvas();
+    drawSquare();
 }
+
+export { getGL }
---
+++ src/vertex_buffer.js
@@ -0,0 +1,29 @@
+"use strict";
+import * as core from "./core.js";
+
+let mGLVertexBuffer = null;
+function get() { return mGLVertexBuffer; }
+
+let mVerticesOfSquare = [
+    0.5, 0.5, 0.0,
+    -0.5, 0.5, 0.0,
+    0.5, -0.5, 0.0,
+    -0.5, -0.5, 0.0
+];
+
+function init() {
+    let gl = core.getGL();
+
+    //Creating a buffer
+    mGLVertexBuffer = gl.createBuffer();
+
+    // Activate vertexBuffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, mGLVertexBuffer);
+
+    // Loads mVerticesOfSquare into the vertexBuffer
+    gl.bufferData(gl.ARRAY_BUFFER, 
+        new Float32Array(mVerticesOfSquare), gl.STATIC_DRAW
+    );
+}
+
+export {init, get}
